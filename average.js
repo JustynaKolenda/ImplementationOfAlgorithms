f83e1c49d84af11a6882527ffe64a07f0d78a005
Xirilla <j.trybalaa@gmail.com>
2017-12-12 21:13:28 +0100

    finish implementation of average

--- average.js
+++ average.js
@@ -1,5 +1,13 @@
 
 var num = [10,11,12,16];
-var allNum = (
-console.log(allNum);
 
+function average() {
+    var numAll = 0;
+    for (i = 0; i < num.length; i++) {
+    numAll += num[i];
+    }
+
+    return(numAll/num.length);
+};
+
+console.log(average(num));
