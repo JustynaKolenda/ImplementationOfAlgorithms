c455213366d0abf407969de461ba40dfa555a6de
Xirilla <j.trybalaa@gmail.com>
2017-12-13 20:20:50 +0100

    Paritity test- done

---
+++ parity.js
@@ -0,0 +1,18 @@
+
+
+function parity(number){
+    var i = number % 2;
+
+    if(i | 0){
+        return "Nie parzysta";
+    } else {
+        return "Parzysta";
+    }
+
+}
+
+function change(){
+
+   number = document.getElementById("numParity").value
+    console.log(parity(number));
+}
